6f5c75731b726668673cc39cc2f96448d54c150e
Alex Caskey <alexcaskey@mwc-005069.dhcp.missouri.edu>
2019-02-15 08:37:22 -0600

    first commit

---
+++ documentChallenge.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace documentChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*display document followed by a blank line*/
+
+            Console.WriteLine("Document");
+
+            Console.WriteLine("");
+
+            /*promt user for a document name*/
+
+            string nameInput;
+            Console.Write("what would you like to name the document");
+            nameInput = Console.ReadLine();
+            Console.WriteLine("you entered", nameInput);
+
+            string contentInput;
+            Console.Write("What is this document for?");
+            contentInput = Console.ReadLine();
+            Console.WriteLine("the content is", contentInput);
+
+            /*make users input a text file*/
+
+            try
+            {
+                TextWriter tw = new StreamWriter("d:/nameInput.txt", true);
+                tw.WriteLine("contentInput");
+                tw.Close();
+                Console.WriteLine(nameInput, "was successfully saved. The document contains", contentInput, "characters.");
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
